6afe6872e4aa266035769224dc20674dcb64f867
botch <33966128+botchGNU@users.noreply.github.com>
2021-08-19 01:08:36 +0200

    Implement login gif

---
+++ clientV2/balloonHarbourClientV2/balloonHarbourClientV2/Classes/LoginGif.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balloonHarbourClientV2.Classes {
+    static class LoginGif {
+        
+        static public string GetRandomLoginGif() {
+            string filepath = "./Resources/Image/Gif/Login/";
+            Directory.CreateDirectory(filepath);
+
+            Properties.Resources.LoginCat3.Save(filepath + "LoginCat3.gif");
+            Properties.Resources.LoginCat2.Save(filepath + "LoginCat2.gif");
+
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+            List<FileInfo> loginGifFiles = new List<FileInfo>();
+
+            foreach (var file in d.GetFiles("*.gif")) {
+                loginGifFiles.Add(file);
+            }
+
+            Random rnd = new Random();
+            return loginGifFiles[rnd.Next(0, loginGifFiles.Count)].FullName;
+        }
+    }
+}
