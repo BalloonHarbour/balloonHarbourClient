465062b1f37bbd05446af45943ac73074afab68f
botch <33966128+botchGNU@users.noreply.github.com>
2021-08-21 14:47:23 +0200

    Save/read user settings (no encryption)

---
+++ clientV2/balloonHarbourClientV2/balloonHarbourClientV2/Classes/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balloonHarbourClientV2.Classes {
+    public class User {
+        private int id;
+        private string username;
+        private Color color;
+        private DateTime lastOnline;
+        private DateTime creationDate;
+        private UserSettings settings;
+
+        public User(int id, string username, Color color, DateTime lastOnline, DateTime creationDate, UserSettings settings) {
+            this.id = id;
+            this.username = username;
+            this.color = color;
+            this.lastOnline = lastOnline;
+            this.creationDate = creationDate;
+            this.settings = settings;
+        }
+
+        public int Id { get => id;}
+        public string Username { get => username; set => username = value; }
+        public Color Color { get => color; set => color = value; }
+        public DateTime LastOnline { get => lastOnline; set => lastOnline = value; }
+        public DateTime CreationDate { get => creationDate;}
+        internal UserSettings Settings { get => settings; set => settings = value; }
+    }
+}
---
+++ clientV2/balloonHarbourClientV2/balloonHarbourClientV2/Classes/UserSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balloonHarbourClientV2.Classes {
+    public class UserSettings {
+        //Client Interface
+        private string theme;
+        //Client
+        private string clientActualVersion;
+        private string autoUpdate;
+        private string operatingSystemLast;
+        private string[] operatingSystems;
+
+        public UserSettings() {
+
+        }
+
+        public string Theme { get => theme; set => theme = value; }
+        public string ClientActualVersion { get => clientActualVersion; set => clientActualVersion = value; }
+        public string AutoUpdate { get => autoUpdate; set => autoUpdate = value; }
+        public string OperatingSystemLast { get => operatingSystemLast; set => operatingSystemLast = value; }
+    }
+}
