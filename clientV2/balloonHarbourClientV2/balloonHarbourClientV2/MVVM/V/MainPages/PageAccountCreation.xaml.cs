dc7b5ea45004efa13a85b5f7a1a73ce4f3bd1e9b
Tohlester <elias.kicker@gmx.at>
2021-08-23 18:25:39 +0200

    pls work

--- clientV2/balloonHarbourClientV2/balloonHarbourClientV2/MVVM/V/MainPages/PageAccountCreation.xaml.cs
+++ clientV2/balloonHarbourClientV2/balloonHarbourClientV2/MVVM/V/MainPages/PageAccountCreation.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +9,9 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using balloonHarbourClientV2.Classes;
-using XamlAnimatedGif;
 
 namespace balloonHarbourClientV2.MVVM.V {
     /// <summary>
@@ -25,17 +21,6 @@ namespace balloonHarbourClientV2.MVVM.V {
         private string pageHeader = "Greetings!";
         public PageAccountCreation() {
             InitializeComponent();
-        }
-
-        public void SetLoginGif() {
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            Uri loginImageUri = new Uri(LoginGif.GetRandomLoginGif());
-            AnimationBehavior.SetSourceUri(ImageLoginGIf, loginImageUri);
-            stopwatch.Stop();
-            MessageBox.Show(Convert.ToString(stopwatch.ElapsedMilliseconds));
         }
 
         public string PageHeader { get => pageHeader; set => pageHeader = value; }
